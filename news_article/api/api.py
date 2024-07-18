1e3173b2a152602329bcfc7f43a1d29f9ef2056c
msploit0x00 <minasploit1@outlook.com>
2024-07-18 10:37:52 +0300

    fixing get_email for committe members

--- news_article/api/api.py
+++ news_article/api/api.py
@@ -11,27 +11,29 @@ def get_committee_mail(gen, customer_status):
     customers = frappe.get_all(
         "Committees you would like to join",
         filters={"committees": ("in", selected_committee)},
+        limit_page_length=5000,
         fields=["parent"])
     filtered_customers = []
+    contacts = []
+
     for customer in customers:
         customer_doc = frappe.get_doc("Customer", customer["parent"])
         if customer_doc.custom_customer_status == customer_status:
             filtered_customers.append(customer_doc.name)
-    contacts_names = frappe.get_all("Dynamic Link",
-                                    filters={
-                                        "link_doctype": "Customer",
-                                        "link_name": ["in", filtered_customers]
-                                    },
-                                    fields=["parent"])
-    contacts = []
-    for contact in contacts_names:
-        if frappe.db.exists("Contact", contact["parent"]):
-            contact_doc = frappe.get_doc("Contact", contact["parent"])
-            if contact_doc.email_id:
-                name = contact_doc.first_name + " " if contact_doc.first_name else ""
-                name += contact_doc.last_name if contact_doc.last_name else ""
+    # contacts_names = frappe.get_all("Dynamic Link",
+    #                                 filters={
+    #                                     "link_doctype": "Customer",
+    #                                     "link_name": ["in", filtered_customers]
+    #                                 },
+    #                                 fields=["parent"])
+    # for contact in contacts_names:
+    #     if frappe.db.exists("Contact", contact["parent"]):
+    #         contact_doc = frappe.get_doc("Contact", contact["parent"])
+    #         if contact_doc.email_id:
+    #             name = contact_doc.first_name + " " if contact_doc.first_name else ""
+    #             name += contact_doc.last_name if contact_doc.last_name else ""
 
-                contacts.append({"email": contact_doc.email_id, "name": name})
+            contacts.append({"email": customer_doc.custom_email, "name": customer_doc.name})
     return contacts
     
     
@@ -85,25 +87,27 @@ def get_committee_mail2(gen, customer_status):
     customers = frappe.get_all(
         "Committees you would like to join",
         filters={"committees": ("in", selected_committee)},
+	limit_page_length=5000,
         fields=["parent"])
     filtered_customers = []
+    contacts = []
     for customer in customers:
         customer_doc = frappe.get_doc("Customer", customer["parent"])
         if customer_doc.custom_customer_status == customer_status:
             filtered_customers.append(customer_doc.name)
-    contacts_names = frappe.get_all("Dynamic Link",
-                                    filters={
-                                        "link_doctype": "Customer",
-                                        "link_name": ["in", filtered_customers]
-                                    },
-                                    fields=["parent"])
-    contacts = []
-    for contact in contacts_names:
-        if frappe.db.exists("Contact", contact["parent"]):
-            contact_doc = frappe.get_doc("Contact", contact["parent"])
-            if contact_doc.email_id:
-                name = contact_doc.first_name + " " if contact_doc.first_name else ""
-                name += contact_doc.last_name if contact_doc.last_name else ""
-
-                contacts.append({"email": contact_doc.email_id, "name": name})
+    # contacts_names = frappe.get_all("Dynamic Link",
+    #                                 filters={
+    #                                     "link_doctype": "Customer",
+    #                                     "link_name": ["in", filtered_customers]
+    #                                 },
+    #                                 fields=["parent"])
+        
+#    for contact in contacts_names:
+#        if frappe.db.exists("Contact", contact["parent"]):
+#            contact_doc = frappe.get_doc("Contact", contact["parent"])
+#            if contact_doc.email_id:
+#                name = contact_doc.first_name + " " if contact_doc.first_name else ""
+#                name += contact_doc.last_name if contact_doc.last_name else ""
+#
+            contacts.append({"email": customer_doc.custom_email, "name": customer_doc.name})
     return contacts
