7484e12f2821386789642a5bb10b4ceda99b6fbb
MinaAshraf67 <minaaashraff670@gmail.com>
2025-11-29 23:10:36 +0200

    test

---
+++ news_article/news_article/doctype/issue_attachments/issue_attachments.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025, mina and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class IssueAttachments(Document):
+	pass
