bd3f90a279e885f02fa4fafac4cb56717333a0b0
msploit0x00 <minasploit1@outlook.com>
2025-11-29 23:19:03 +0200

    solving conflicts

--- news_article/news_article/doctype/issue_assignment/issue_assignment.py
+++ news_article/news_article/doctype/issue_assignment/issue_assignment.py
@@ -1,9 +1,8 @@
-
-# Copyright (c) 2025, mina and contributors
+# Copyright (c) 2024, mina and contributors
 # For license information, please see license.txt
 
 # import frappe
 from frappe.model.document import Document
 
-class linkedissues(Document):
+class IssueAssignment(Document):
 	pass
---
+++ news_article/news_article/doctype/issue_priority/issue_priority.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, mina and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Issue Priority', {
+	// refresh: function(frm) {
+
+	// }
+});
